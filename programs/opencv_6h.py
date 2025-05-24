a93210b56ce019ca6bdfc5399ec41d5333a76f01
SARPAT <saranshappy@gmail.com>
2025-05-24 23:09:36 +0530

    parse using function

--- programs/opencv_6h.py
+++ programs/opencv_6h.py
@@ -5,6 +5,7 @@ import mediapipe as mp
 width=640
 height=360
 
+#my_hands =mp.solutions.hands
 hands = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) #false=not a static image , 2= for two hands , 0.5=confidence in tracking 
 #draw object
 mpDraw=mp.solutions.drawing_utils
@@ -14,9 +15,9 @@ def parseLandmarks(frame):
     frameRGB=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     results=hands.process(frameRGB)
     if results.multi_hand_landmarks !=None :
-        for handlandmarks in results.multi_handlandmarks :
+        for handlandmarks in results.multi_hand_landmarks :
             myhand=[]
-            mpDraw.draw_Landmarks(frame,handlandmarks,mp.solutions.hands.HAND_CONNECTIONS)
+            mpDraw.draw_landmarks(frame,handlandmarks,mp.solutions.hands.HAND_CONNECTIONS)
             for landmarks in handlandmarks.landmark:
                 myhand.append((int(landmarks.x*width),int(landmarks.y*height)))
             myhands.append(myhand)
@@ -32,9 +33,13 @@ cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
 while True:
     ignore,frame=cam.read()
+    #resizing is an important aspect , always resize to width and height 
+    frame=cv2.resize(frame,(width,height))
+
     myHands=parseLandmarks(frame)
     for hand in myHands:
-        cv2.circle(frame,hand[20],10,(255,0,0),-1)
+        for tips in [0,4,8,12,16,20]:
+         cv2.circle(frame,hand[tips],10,(55,100,0),2)
     cv2.imshow('my WEBcam', frame) 
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
